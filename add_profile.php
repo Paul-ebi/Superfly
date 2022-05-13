4fff482d9319d71143698f5515ed9909c7acfb53
Paul-ebi <31934858+Paul-ebi@users.noreply.github.com>
2022-05-13 15:56:25 -0700

    Update add_profile.php
    updates made

--- add_profile.php
+++ add_profile.php
@@ -2,30 +2,31 @@
 include_once 'dbConfig.php';
 if(isset($_POST['submit']))
 {    
-     $fullname = $_POST['fullname'];
-     $email = $_POST['email'];
-     $address = $_POST['c_address'];
-     $phone = $_POST['phone'];
-     $marital_status = $_POST['marital_status'];
-     $occupants = $_POST['occupants'];
-     $duration = $_POST['duration'];
-     $date_of_entry = $_POST['date_of_entry'];
-     $smoker = $_POST['smoker'];
-     $pets = $_POST['pets'];
-     $dob = $_POST['dob'];
-     $ssn = $_POST['ssn'];
-     $credit_score = $_POST['credit_score'];
-     $sql = "INSERT INTO client (fullname,email,c_address,phone,marital_status,occupants,duration,date_of_entry,smoker,pets,dob,ssn,credit_score)
-     VALUES ('$fullname','$email','$address','$phone','$marital_status','$occupants','$duration','$date_of_entry','$smoker','$pets','$dob','$ssn','$credit_score')";
+   
+   $firstName = $_POST['firstname'];
+   $middleName = $_POST['middlename'];
+   $lastName = $_POST['lastname'];
+   $emails = $_POST['email'];   
+   $dob = $_POST['dob'];   
+   $address1 = $_POST['c_address1'];
+   $address2 = $_POST['c_address2'];
+   $city = $_POST['city'];
+   $country = $_POST['country_of_residence'];
+   $phoneLine = $_POST['phone'];
+   $maritalStatus = $_POST['marital_status'];
+   $nationality = $_POST['nationality'];
+     $sql = "INSERT INTO profiles (f_name, m_name, l_name, email_add,  dob, address1, address2, city, country_of_residence, phone_line, marital_status, nationality)
+     VALUES ('$firstName', '$middleName', '$lastName', '$emails', '$dob', '$address1', '$address2','$city','$country','$phoneLine','$maritalStatus', '$nationality')";
      if (mysqli_query($conn, $sql)) {
-        echo '<script>alert("New record was added successful!")</script>';
+        echo '<script>alert("New record was added successful!");
+        window.location.href = "user_home.php";</script>';
      } else {
         echo "Error: " . $sql . ":-" . mysqli_error($conn);
      }
      mysqli_close($conn);
 }
 
-include_once 'dbConfig.php';
+/*include_once 'dbConfig.php';
 // File upload path
 if(isset($_FILES['file_array'])){
     $name_array = $_FILES['file_array']['name'];
@@ -44,7 +45,7 @@ if(isset($_FILES['file_array'])){
            echo "upload file failed to move for". $name_array[$i]."<br>";
        }
        }
-    }
+    }*/
 
 
 ?>
